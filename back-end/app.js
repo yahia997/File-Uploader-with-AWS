487d27f4fb12d2a3b14b2e4f58d64029cf367ac4
Yahia mahmoud <yahiamahmoood333@gmail.com>
2025-04-23 11:33:30 +0200

    Connect with S3

--- back-end/app.js
+++ back-end/app.js
@@ -4,23 +4,50 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
+// to load system variables
+dotenv.config();
+
 const app = express();
 const port = 8000;
 app.use(cors());
 app.use(express.json());
 
 // Connect ot S3
-// const s3Client = new S3Client({
-//   region: process.env.AWS_REGION,
-//   credentials: {
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//   },
-// });
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
 
 // just for testing
 app.get('/hello', (req, res) => {
   res.json('{"message": "wadddddyyyy"}');
+});
+
+// test s3
+app.get('/api/files', async (req, res) => {
+  try {
+    const command = new ListObjectsV2Command({
+      Bucket: process.env.AWS_BUCKET_NAME
+      // Prefix: '/'
+    });
+
+    const { Contents } = await s3Client.send(command);
+    const files = Contents
+      .filter(item => item.Size > 0)
+      .map(item => ({
+        name: item.Key.replace('uploads/', ''),
+        size: item.Size,
+        lastModified: item.LastModified
+      }));
+
+    res.json(files);
+  } catch (error) {
+    console.error('Error listing files:', error);
+    res.status(500).json({ error: 'Failed to list files' });
+  }
 });
 
 app.listen(port ,() => {
