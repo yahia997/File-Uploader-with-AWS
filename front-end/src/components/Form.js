0064ced9371c53459a4e8ae6f72be7e6ad6a9a21
Yahia mahmoud <yahiamahmoood333@gmail.com>
2025-04-23 22:43:18 +0200

    add pdf txt jpg png only

--- front-end/src/components/Form.js
+++ front-end/src/components/Form.js
@@ -20,42 +20,46 @@ export default function Form({setUploading, uploading, path}) {
   
   async function handleSubmit(e) {
     e.preventDefault();
+    let s = new Set(["application/pdf", "image/jpeg", "image/jpg", "image/png", "text/plain"]);
+    
+    if(s.has(fileType)) {
 
-    const obj = {
-      fileName: fileName,
-      fileType: fileType
+      const obj = {
+        fileName: fileName,
+        fileType: fileType
+      }
+      
+      // To get link for upload
+      var res = await fetch(`http://localhost:8000/api/generate-upload-url`, {
+          method: "POST",
+          headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(obj)
+          });
+          
+          if (!res.ok) {
+              throw new Error('Failed to get upload URL');
+            }
+            
+            const { uploadURL } = await res.json();
+
+            // Step 2: Upload directly to S3 using the presigned URL
+            const uploadResponse = await fetch(uploadURL, {
+                method: 'PUT',
+                body: file,
+                headers: {
+                    'Content-Type': file.type,
+                  },
+                });
+                
+                if (!uploadResponse.ok) {
+                    throw new Error('Upload failed');
+                  }
+                  
+                  console.log("Successfully uploaded !!!");
+                  setUploading(!uploading);
     }
-
-    // To get link for upload
-    var res = await fetch(`http://localhost:8000/api/generate-upload-url`, {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(obj)
-    });
-
-    if (!res.ok) {
-      throw new Error('Failed to get upload URL');
-    }
-
-    const { uploadURL } = await res.json();
-
-    // Step 2: Upload directly to S3 using the presigned URL
-    const uploadResponse = await fetch(uploadURL, {
-      method: 'PUT',
-      body: file,
-      headers: {
-        'Content-Type': file.type,
-      },
-    });
-
-    if (!uploadResponse.ok) {
-      throw new Error('Upload failed');
-    }
-
-    console.log("Successfully uploaded !!!");
-    setUploading(!uploading);
   }
 
   return (
